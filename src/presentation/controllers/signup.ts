b7e0bbe9854caf767f4abf91a5faa1887482e3f0
Luan Persini <luanpersini@hotmail.com>
2021-05-30 20:24:55 -0300

    feat: ensure SignUpController passwordConfirmation returns badrequest if fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -14,7 +14,7 @@ export class SignUpController implements Controller {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
-          return badRequest(MissingParamError(field))
+          return badRequest(new MissingParamError(field))
         }
       }
       if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
