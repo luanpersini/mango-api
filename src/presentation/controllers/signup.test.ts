f5417c200290adca9a96bc00d4ab2543315d1f45
Luan Persini <luanpersini@hotmail.com>
2021-05-30 00:14:27 -0300

    feat: should return 400 if no email is provided

--- src/presentation/controllers/signup.test.ts
+++ src/presentation/controllers/signup.test.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signup'
 
 describe('SignUpController', () => {
-  test('Should return 400 if AddAccount throws', async () => {
+  test('should return 400 if no name is provided', async () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -13,5 +13,19 @@ describe('SignUpController', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('should return 400 if no email is provided', async () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
