32746c4e0713846565b4c0c3dd09cdc48a4d8b6e
Luan Persini <luanpersini@hotmail.com>
2021-06-07 17:55:44 -0300

    feat: test + ensure content-type is json by default

--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -1,8 +1,8 @@
 export * from './body-parser'
 export * from './cors'
+export * from './content-type'
 
 /*
-export * from './content-type'
 export * from './no-cache'
 export * from './auth'
 export * from './admin-auth'
