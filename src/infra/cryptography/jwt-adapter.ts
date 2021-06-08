0ee3277e737211e8f4cca519338a0e59d3481c07
Luan Persini <luanpersini@hotmail.com>
2021-06-08 02:57:56 -0300

    chore: configuring import/no-unresolved

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -1,12 +1,13 @@
 import { Encrypter, Decrypter } from '@/data/protocols'
 
 import jwt from 'jsonwebtoken'
+import jwt2 from 'jsonwebtoken2'
 
 export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (plaintext: string): Promise<string> {
-    return jwt.sign({ id: plaintext }, this.secret)
+    return jwt2.sign({ id: plaintext }, this.secret)
   }
 
   async decrypt (ciphertext: string): Promise<string> {
