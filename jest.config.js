519c00158913f3345bca5bd4c6c647026f68f2de
Luan Persini <luanpersini@hotmail.com>
2021-06-20 00:49:49 -0300

    chore: versão 2.0.0 funcionando, junto com import/no-unresolved

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!**/index.ts', '!**/helpers/**.ts', '!<rootDir>/src/**/**-protocols.ts', '!**/server.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!**/index.ts', '!<rootDir>/src/**/**-protocols.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+  '@/(.*)': '<rootDir>/src/$1'
   }
 }
