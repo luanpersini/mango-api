33d14a73372d7737d5391bd7a0b1561d0b33f841
Luan Persini <lpersini@pmcgroupone.com>
2023-09-08 18:36:51 -0300

    adjustment commit

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!**/index.ts', '!<rootDir>/src/**/**-protocols.ts',  '!**/test/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+  '@/(.*)': '<rootDir>/src/$1'
+  }
+}
